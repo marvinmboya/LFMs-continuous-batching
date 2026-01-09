97dd10ce75da4fea1f0a70c3751ec837ed6e36d9
Marvin-desmond <marvindesmond@gmail.com>
2026-01-09 10:43:27 -0500

    time token decode in terms of seconds taken per token

--- lfm_decode.py
+++ lfm_decode.py
@@ -1,4 +1,5 @@
 import torch 
+import time 
 
 def sample_next_token(logits, top_k=None, temperature=1.0):
     if top_k is not None:
@@ -13,16 +14,21 @@ def sample_next_token(logits, top_k=None, temperature=1.0):
         next_token_id = torch.argmax(logits, dim=-1, keepdim=True)
     return next_token_id
 
+colorprint = lambda x: print(f"\n\x1B[0;33m[{x} tokens/second]\x1B[0m")
 def decode_next_token(
         model, tokenizer, init_tokens, eos_token_id, 
         top_k=None, temperature=1.0, max_tokens=10,
     ):
     token_ids = init_tokens
     logits = model(init_tokens)[:, -1]
-    for _ in range(max_tokens):
+
+    avg_seconds_per_token = 0.0
+    cum_avgs = []
+    for index in range(max_tokens):
         next_token_id = sample_next_token(
             logits, top_k, temperature
         )
+        start = time.perf_counter()
         if (eos_token_id is not None and 
             torch.all(next_token_id == eos_token_id)):
             break
@@ -31,3 +37,10 @@ def decode_next_token(
         print(next_token, flush=True, end="")
         token_ids = torch.cat((token_ids, next_token_id), dim=1)
         logits = model(token_ids)[:, -1]
+        end = time.perf_counter()
+        avg_seconds_per_token = (1/(index + 1)) * (
+            (index * avg_seconds_per_token) + (end - start)
+        )
+        cum_avgs.append(avg_seconds_per_token)
+    colorprint(f"DECODE: {1/avg_seconds_per_token:.1f}")
+    torch.save(torch.tensor(cum_avgs), "avgs_seconds_per_token.pt")
