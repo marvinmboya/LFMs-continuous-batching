e468948a2d097763e16262df2069a5fb756d226d
Marvin-desmond <marvindesmond@gmail.com>
2026-01-09 19:17:11 -0500

    propagating hybrid cache instance into gsc (computing stateful conv_out)

--- gsc.py
+++ gsc.py
@@ -1,3 +1,4 @@
+import torch 
 import torch.nn as nn 
 from functools import partial 
 from lfm_config import LFM2Config
@@ -16,12 +17,31 @@ class GatedShortBlock(nn.Module):
         lin = partial(nn.Linear, bias=False, dtype=dtype)
         self.w1 = lin(d_model, 3 * d_model)
         self.w2 = lin(d_model, d_model)
+        self.k_size = k_size
 
-    def forward(self, x):
+    def forward(self, x, l_idx, hybrid_cache, cache_pos_ids):
+        k_size = self.k_size
+
         seq_len = x.shape[1] 
         BCx = self.w1(x).transpose(-1, -2)
         B, C, x = BCx.chunk(3, dim=-2)
-        conv_out = self.conv(B * x)[..., :seq_len]
+        Bx = B * x
+        if cache_pos_ids[0] > 0:
+            conv_state = hybrid_cache.conv_cache[l_idx]
+            cache_pos_ids = cache_pos_ids.clamp(0, k_size - 1)
+            conv_state = conv_state.roll(shifts=-1, dims=-1)
+            conv_state[:, :, cache_pos_ids] = Bx.to(
+                device=conv_state.device, dtype=conv_state.dtype
+            )
+            hybrid_cache.conv_cache[l_idx].copy_(conv_state)
+            conv_out = torch.sum(
+                conv_state.to(Bx.device) * self.conv.weight[:, 0, :], dim=-1
+            )
+            conv_out = conv_out.unsqueeze(-1)
+        else:
+            conv_state = nn.functional.pad(Bx, (k_size - Bx.shape[-1], 0))
+            hybrid_cache.conv_cache[l_idx].copy_(conv_state)
+            conv_out = self.conv(Bx)[..., :seq_len]
         x = C * conv_out
         x = x.transpose(-1, -2).contiguous()
         return self.w2(x)
