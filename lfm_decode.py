e7906007ca017b2a764d06f25f3f92be8fc7bcf8
Marvin-desmond <marvindesmond@gmail.com>
2026-01-11 22:08:31 -0500

    compute ragge prefill, repack kv cache, and set values for compact decoding

--- lfm_decode.py
+++ lfm_decode.py
@@ -15,13 +15,23 @@ def sample_next_token(logits, top_k=None, temperature=1.0):
     return next_token_id
 
 def decode_next_token(
-    model, init_tokens, other_prompts, tokenizer, hybrid_cache, 
+    model, ragged_mask, prompt_breaks, init_tokens, other_prompts, tokenizer, hybrid_cache, 
     eos_token_id, top_k=None, temperature=1.0, max_tokens=10, done=None):
     token_ids = init_tokens
-    logits = model(init_tokens, hybrid_cache)[:, -1]
-
+    logits = model(init_tokens, ragged_mask, hybrid_cache)
+    next_logits = []
+    for i in range(len(prompt_breaks)-1):
+        last_idx = prompt_breaks[i + 1] - 1
+        next_logits.append(logits[0, last_idx].unsqueeze(0))
+    next_logits = torch.cat(next_logits, dim=0)
+    next_token_id = sample_next_token(next_logits, top_k, temperature)
+    _next_token_id = next_token_id.flatten().tolist()
+    hybrid_cache.repack_kv_caches(prompt_breaks)
+    logits = model(next_token_id, None, hybrid_cache)[:, -1]
+    prompt_meta = (0, False, "")
     avg_seconds_per_token = 0.0
-    seq_len = init_tokens.size(1)
+    yield _next_token_id, prompt_meta, avg_seconds_per_token
+    seq_len = hybrid_cache.k_cache[2].size(2)
     for index in range(max_tokens):
         prompt_meta = (0, False, "")
         next_token_id = sample_next_token(
@@ -44,7 +54,7 @@ def decode_next_token(
                     new_batch_id[0, st:st + sz] = new_token_id
                     hybrid_cache.is_inter = True
                     hybrid_cache.inter_batch = i
-                    logits = model(new_batch_id, hybrid_cache)[:, -1]
+                    logits = model(new_batch_id, None, hybrid_cache)[:, -1]
                     hybrid_cache.is_inter = False 
                     hybrid_cache.inter_batch = 0
                     new_prefill_id = sample_next_token(logits, top_k, temperature)
@@ -58,8 +68,7 @@ def decode_next_token(
             torch.all(next_token_id == eos_token_id)):
             break
         _next_token_id = next_token_id.flatten().tolist()
-        token_ids = torch.cat((token_ids, next_token_id), dim=1)
-        logits = model(next_token_id, hybrid_cache)[:, -1]
+        logits = model(next_token_id, None, hybrid_cache)[:, -1]
         end = time.perf_counter()
         avg_seconds_per_token = (1/(index + 1)) * (
             (index * avg_seconds_per_token) + (end - start)
