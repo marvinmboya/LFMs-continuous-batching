d24b9e3585be567af20dde1a5dd9f3fac845a29c
Marvin-desmond <marvindesmond@gmail.com>
2026-02-08 17:31:10 -0500

    pass overriding prompt via cmd, conditionally run model download script

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import torch
+import argparse 
+from pathlib import Path 
+
 from lfm_cache import HybridCache
 
 from lfm_tokenizer import Lfm2Tokenizer
@@ -6,7 +9,11 @@ from lfm_decode import decode_next_token
 from lfm_config import LFM2Config
 from lfm_arch import LFM2350M
 
-prompt = "The ruler of a kingdom is a"
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--prompt', type=str, default="What is hello in spanish?")
+args = parser.parse_args()
+
+prompt = args.prompt
 tokenizer = Lfm2Tokenizer("tokenizer.json")
 encoded_prompt = tokenizer.encode(prompt)
 encoded_prompt_d = torch.tensor(
@@ -21,12 +28,13 @@ from huggingface_hub import hf_hub_download
 from safetensors.torch import load_file
 from lfm_weight import transferLFMWeights
 
-hf_hub_download(
-    repo_id = "LiquidAI/LFM2-350M",
-    local_dir = "./",
-    filename = "model.safetensors",
-    revision="3dbef32"
-)
+if not Path("./model.safetensors").exists():
+    hf_hub_download(
+        repo_id = "LiquidAI/LFM2-350M",
+        local_dir = "./",
+        filename = "model.safetensors",
+        revision="3dbef32"
+    )
 
 pretrained_state_dict = load_file("model.safetensors")
 transferLFMWeights(model, pretrained_state_dict)
@@ -39,5 +47,5 @@ hybrid_cache = HybridCache(LFM2Config, batch_size, LFM2Config.dtype, device)
 with torch.no_grad():
     decode_next_token(
         model, tokenizer, encoded_prompt_d, hybrid_cache,
-        tokenizer.eos_token_id, temperature=0.3, max_tokens=100
+        tokenizer.eos_token_id, temperature=0.3, max_tokens=500
     )
